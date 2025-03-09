e7791f85a6eb9e5e51aa907fda09b5a69a128c87
Ilayda Yilmaz <183476140+Ilayda-git@users.noreply.github.com>
2025-03-09 15:51:59 +0100

    Delete Mon-site-web/scripts directory

--- Mon-site-web/scripts/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Le site est chargé !");
-    const links = document.querySelectorAll("nav ul li a");
-    links.forEach(link => {
-        link.addEventListener("click", (e) => {
-            alert(`Vous avez cliqué sur ${link.textContent}`);
-        });
-    });
-});
